4aa68d41c08e61092b5a99bc836fd722615be694
afk <adamfk@gmail.com>
2021-06-27 00:47:20 -0500

    start regex parsing for statemachine roots

---
+++ cs-generator/StateSmith/StateSmith/Hsm.cs
@@ -0,0 +1,64 @@
+﻿using StateSmith.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StateSmith
+{
+
+    class HsmState
+    {
+        public DiagramNode diagramNode;
+    }
+
+    class HsmTransition
+    {
+        public DiagramEdge diagramEdge;
+    }
+
+
+    class Hsm
+    {
+        public HsmState rootState;
+    }
+
+
+
+    public class NodeLabelTextParser
+    {
+        //Find state machine nodes
+        public static readonly Regex statemachineRegex = new Regex(@"
+            (?i)
+            ^\s*
+            [$]STATEMACHINE
+            \s*
+            :
+            \s*
+            (?<statemachine_name> \w+ )
+            \s*
+            (?:$|\r|\n)
+        ", RegexOptions.IgnorePatternWhitespace);
+
+        public (string statemachineName, int nextParseIndex)? TryMatchStatemachine(string nodeLabelText)
+        {
+            Match match = statemachineRegex.Match(nodeLabelText);
+            if (!match.Success)
+            {
+                return null;
+            }
+
+            return (match.Groups["statemachine_name"].Value, match.Length);
+        }
+    }
+
+    class DiagramToHsmConverter
+    {
+        public List<Hsm> Convert(List<DiagramNode> diagramNodes)
+        {
+            //need to parse trees from each root
+            return null;
+        }
+    }
+
+}
---
+++ cs-generator/StateSmith/StateSmithTest/NodeLabelTextParserTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using StateSmith;
+
+namespace StateSmithTest
+{
+    public class NodeLabelTextParserTest
+    {
+        [Fact]
+        public void TestStatemachineParse()
+        {
+            var parser = new NodeLabelTextParser();
+            var possibleMatch = parser.TryMatchStatemachine("$STATEMACHINE:BurritoSm");
+            possibleMatch.HasValue.Should().BeTrue();
+            var (statemachineName, nextParseIndex) = possibleMatch.Value;
+            statemachineName.Should().Be("BurritoSm");
+            nextParseIndex.Should().Be(23);
+        }
+    }
+}
